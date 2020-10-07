78d73adfb1f5a00fc5e5392dd0717d34928413c9
kimwoobin <kwb020312@naver.com>
2020-10-07 22:02:28 +0900

    Updates

---
+++ precache-manifest.35d81eef125a7450616b6f47d0652ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9d979555d39e81aa37c01e28683146",
+    "url": "/Nwitter/index.html"
+  },
+  {
+    "revision": "bc8aaf74f65006f25629",
+    "url": "/Nwitter/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "acf3e30d2ec3a0a18221",
+    "url": "/Nwitter/static/js/2.50f048be.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/Nwitter/static/js/2.50f048be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc8aaf74f65006f25629",
+    "url": "/Nwitter/static/js/main.c87f0b26.chunk.js"
+  },
+  {
+    "revision": "06c4033b76c035e967b8",
+    "url": "/Nwitter/static/js/runtime-main.fcdd3e8e.js"
+  }
+]);
